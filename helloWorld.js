88d6e84d02a6391ceeb5bfa61810ae95fc230c68
jfcarocota <jfcarocota@gmail.com>
2019-10-30 09:49:50 -0700

    compile shader

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,40 @@
 const canvas = document.getElementById('glcanvas');
 
+const vsSource = `
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+`;
+
+const fsSource = `
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`;
+
+const loadShader = (gl, type, source)=>{
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+const initShader = (gl, vsSource, fsSource)=>{
+
+}
+
 const main = ()=>{
     const gl = canvas.getContext('webgl');
 
@@ -8,6 +43,7 @@ const main = ()=>{
         return;
     }
 
+    initShader(gl, vsSource, fsShader)
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
