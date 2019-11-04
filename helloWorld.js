208282ca0dde9dde5cab4afbda7a444abf8610c6
jfcarocota <jfcarocota@gmail.com>
2019-11-04 09:55:30 -0700

    shader program and buffers working

--- helloWorld.js
+++ helloWorld.js
@@ -34,17 +34,71 @@ const loadShader = (gl, type, source)=>{
 const initShader = (gl, vsSource, fsSource)=>{
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    const shaderProgram = gl.createProgram();
+
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+
+const initBuffers = gl =>{
+    
+    const positionBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    const positions = [
+        -1.0,  1.0,
+         1.0,  1.0,
+        -1.0, -1.0,
+         1.0, -1.0,
+      ];
+
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(positions),
+        gl.STATIC_DRAW
+    );
+
+    return {
+        position: positionBuffer
+    };
 }
 
 const main = ()=>{
-    const gl = canvas.getContext('webgl');
+    const gl = canvas.getContext('webgl2');
 
     if (!gl) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
 
-    initShader(gl, vsSource, fsSource)
+    const shaderProgram = initShader(gl, vsSource, fsSource);
+
+    const programInfo = {
+        program : shaderProgram,
+        attribLocations : {
+            vertexPosition : gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+        },
+        uniformLocations : {
+            projectionMatrix : gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMAtrix : gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
+        }
+    };
+
+
+    const buffers = initBuffers(gl);
+
+
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
