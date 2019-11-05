4e5cb0f6290034050b2f2010374713e1f4667e2b
jfcarocota <jfcarocota@gmail.com>
2019-11-05 12:43:54 -0700

    perspective created

--- helloWorld.js
+++ helloWorld.js
@@ -75,12 +75,31 @@ const initBuffers = gl =>{
 }
 
 const drawScene = (gl, programInfo, buffers)=>{
+
+    /* render */
     gl.clearColor(0, 0, 0, 1);
     gl.clearDepth(1);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    /*Camera */
+
+    const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100;
+
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(
+        projectionMatrix,
+        fieldOfView,
+        aspect,
+        zNear,
+        zFar
+    );
 }
 
 const main = ()=>{
